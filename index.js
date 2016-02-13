ab9558a3e413153d729fe06276fbf94a44532854
Vlad Rafeev <vlad19@gmail.com>
2016-02-14 01:07:47 +0200

    server started

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ max.bind();
 
 var clipsLength = undefined;
 var clipNames = [];
+var clipPlayingPosition;
 
 // Get clip name in track 1
 // Let's call this function getClipName
@@ -37,4 +38,20 @@ max.observe({
 })
 .on('value', function(val) {
   console.log('Playing position: ' + val);
+  clipPlayingPosition = val;
 });
+
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end('Playing position: ' + clipPlayingPosition + '\n');
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server started at http://127.0.0.1:8000/");
